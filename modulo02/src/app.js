21fa8847284b8084e0dba17b8b7dae39baa5ca6b
v1eira <ewersonv@gmail.com>
2019-07-19 22:28:19 -0300

    finish modulo02 (Iniciando back-end do GoBarber)

--- modulo02/src/app.js
+++ modulo02/src/app.js
@@ -1,21 +1,23 @@
 import express from 'express';
 import routes from './routes';
 
+import './database';
+
 class App {
-    constructor() {
-        this.server = express();
-        
-        this.middlewares();
-        this.routes();
-    }
+  constructor() {
+    this.server = express();
 
-    middlewares() {
-        this.server.use(express.json());
-    }
+    this.middlewares();
+    this.routes();
+  }
 
-    routes() {
-        this.server.use(routes);
-    }
+  middlewares() {
+    this.server.use(express.json());
+  }
+
+  routes() {
+    this.server.use(routes);
+  }
 }
 
 export default new App().server;
