357d8a2c8be5ad3ee5b93118b86c07489df8be62
v1eira <ewersonv@gmail.com>
2019-08-19 20:59:42 -0300

    att modulo05 (Estilizando página Main)

---
+++ modulo05/src/pages/Main/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import { FaGithubAlt, FaPlus } from 'react-icons/fa'
+
+import { Container, Form, SubmitButton } from './styles';
+
+export default function Main() {
+  return (
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repositories
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input
+          type="text"
+          placeholder="Add repository"
+        />
+
+        <SubmitButton disabled>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+
+    </Container>
+  );
+}
